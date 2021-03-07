e36b542bb96aa656236fc65464cb0819496d3a9a
Искаков Дмитрий Сергеевич <@lcoast.itstep>
2021-03-07 11:12:55 +0600

    Первый урок

---
+++ C#/Program #1.cs
@@ -0,0 +1,33 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		static void Main(string[] args){
+			/*
+	    Console.WriteLine("Сколько вам лет ?");
+	
+			int age = Convert.ToInt32(Concole.ReadLine());
+		
+			//string s_age = Convert.ToString (age)l;
+		
+			Console.WeadLine("Ваш возвраст" + age );
+       if(age > 18){
+			Console.WriteLine("Вы большой");
+	   }
+	   
+		*/
+static void Main(string[] args){	
+int arr = Main(string[] args){
+	var arr = new List<int>{0,1,2,3,4,5,6,7};
+	
+	
+	foreacg()int num in arr){
+	Console.Writeline(num);
+	for (int i = 0; i < 10; i++){		}
+	 Console.Writeline(i);  
+	
+   }
+
+}
